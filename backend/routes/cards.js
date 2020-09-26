ef16e4b0d900f9fede93d9555b55c8eee19247f1
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-25 19:52:20 -0700

    rename

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,28 +1,78 @@
-const router = require('express').Router();
-const Card = require('../models/card.model');
+const router = require("express").Router();
+const Card = require("../models/card.model");
 
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
   Card.find()
-    .then(cards => res.json(cards))
-    .catch(err => res.status(400).json(`Error: ${err}`));
-})
+    .then((cards) => res.json(cards))
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
 
-router.route('/add').post((req, res) => {
-  const {book, deck, korean, english, pronunciation, hanja, onMaster} = req.body;
+router.route("/add").post((req, res) => {
+  const {
+    book,
+    deck,
+    korean,
+    english,
+    pronunciation,
+    hanja,
+    onMaster,
+  } = req.body;
 
-  const newCard = new Card({  
+  const newCard = new Card({
     book,
     deck: Number(deck),
     korean,
     english,
     pronunciation,
     hanja,
-    onMaster: Boolean(deck),
+    onMaster: Boolean(onMaster),
   });
 
-  newCard.save()
-    .then(card => res.status(201).json(card))
-    .catch(err => res.status(400).json(`Error: ${err}`));
-})
+  newCard
+    .save()
+    .then(() => res.status(201).json("Card created."))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route("/:id").get((req, res) => {
+  Card.findById(req.params.id)
+    .then((card) => res.status(200).json(card))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Card.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).json("Card deleted"))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  const {
+    book,
+    deck,
+    korean,
+    english,
+    pronunciation,
+    hanja,
+    onMaster,
+  } = req.body;
+  Card.findById(req.params.id)
+    .then((card) => {
+      card.book = book;
+      card.deck = Number(deck);
+      card.korean = korean;
+      card.english = english;
+      card.pronunciation = pronunciation;
+      card.hanja = hanja;
+      card.onMaster = Boolean(onMaster);
+
+      card
+        .save()
+        .then(() => res.status(200).json("Card updated."))
+        .catch((err) => res.status(400).json(err));
+    })
+    .catch((err) => res.status(400).json(err));
+});
 
 module.exports = router;
+11;
