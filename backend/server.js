3befb1eb75640d06c59878e998df6b89766b0bd0
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-26 22:04:14 -0700

    useUnifiedTopology

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.LOCAL_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }, () => {
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }, () => {
   console.log("MongoDB database connection established successfully.");
 });
 
