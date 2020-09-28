8dc2a8233867ae0e3f1018046234103b78c9af50
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-09-28 11:59:24 -0700

    card routes init, get add

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,78 +1,25 @@
+const pool = require("../config/keys");
+
 const router = require("express").Router();
-const Card = require("../models/card.model");
 
-router.route("/").get((req, res) => {
-  Card.find()
-    .then((cards) => res.json(cards))
-    .catch((err) => res.status(400).json(`Error: ${err}`));
-});
+router.post("/add", async (req, res) => {
+  try {
+    const {book, deck, korean, english, pronunciation, hanja, onMaster} = req.body;
+    const newCard = await pool.query("INSERT INTO cards (book, deck, korean, english, pronunciation, hanja, onMaster) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *", 
+      [book, deck, korean, english, pronunciation, hanja, onMaster])
+    res.send(newCard.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
-router.route("/add").post((req, res) => {
-  const {
-    book,
-    deck,
-    korean,
-    english,
-    pronunciation,
-    hanja,
-    onMaster,
-  } = req.body;
-
-  const newCard = new Card({
-    book,
-    deck: Number(deck),
-    korean,
-    english,
-    pronunciation,
-    hanja,
-    onMaster: Boolean(onMaster),
-  });
-
-  newCard
-    .save()
-    .then(() => res.status(201).json("Card created."))
-    .catch((err) => res.status(400).json(err));
-});
-
-router.route("/:id").get((req, res) => {
-  Card.findById(req.params.id)
-    .then((card) => res.status(200).json(card))
-    .catch((err) => res.status(400).json(err));
-});
-
-router.route("/:id").delete((req, res) => {
-  Card.findByIdAndDelete(req.params.id)
-    .then(() => res.status(200).json("Card deleted"))
-    .catch((err) => res.status(400).json(err));
-});
-
-router.route("/update/:id").post((req, res) => {
-  const {
-    book,
-    deck,
-    korean,
-    english,
-    pronunciation,
-    hanja,
-    onMaster,
-  } = req.body;
-  Card.findById(req.params.id)
-    .then((card) => {
-      card.book = book;
-      card.deck = Number(deck);
-      card.korean = korean;
-      card.english = english;
-      card.pronunciation = pronunciation;
-      card.hanja = hanja;
-      card.onMaster = Boolean(onMaster);
-
-      card
-        .save()
-        .then(() => res.status(200).json("Card updated."))
-        .catch((err) => res.status(400).json(err));
-    })
-    .catch((err) => res.status(400).json(err));
-});
+router.get("/", async(req, res) => {
+  try {
+    const allCards = await pool.query("SELECT * FROM cards");
+    res.json(allCards.rows)
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 module.exports = router;
-11;
