5fe58464ebafd25fde59565d83f3a589cbc997d7
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-07 21:30:38 -0700

    update card call, return into table row

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,19 +1,20 @@
 import React, { useContext, useState, useEffect } from "react";
 import CardsContext from "../../context/cards-context";
 import InlineEdit from "../InlineEdit/InlineEdit";
+import "./Card.scss";
 
 const Card = (props) => {
   const { card } = props;
 
   const { dispatch } = useContext(CardsContext);
 
-  const [ book, setBook ] = useState(card.book);
-  const [ deck, setDeck ] = useState(card.deck);
-  const [ korean, setKorean ] = useState(card.korean);
-  const [ english, setEnglish ] = useState(card.english);
-  const [ pronunciation, setPronunciation ] = useState(card.pronunciation);
-  const [ hanja, setHanja ] = useState(card.hanja);
-  const [ onmaster, setOnMaster ] = useState(card.onmaster);
+  const [book, setBook] = useState(card.book);
+  const [deck, setDeck] = useState(card.deck);
+  const [korean, setKorean] = useState(card.korean);
+  const [english, setEnglish] = useState(card.english);
+  const [pronunciation, setPronunciation] = useState(card.pronunciation);
+  const [hanja, setHanja] = useState(card.hanja);
+  const [onmaster, setOnMaster] = useState(card.onmaster);
 
   useEffect(() => {
     updateCard(card.card_id);
@@ -22,22 +23,39 @@ const Card = (props) => {
 
   const updateCard = async (card_id) => {
     try {
-      const body = {book, deck, korean, english, pronunciation, hanja, onmaster};
-      const uri = `http://localhost:5000/cards/update/${card_id}`
+      const body = {
+        book,
+        deck,
+        korean,
+        english,
+        pronunciation,
+        hanja,
+        onmaster,
+      };
+      const uri = `http://localhost:5000/cards/update/${card_id}`;
       const options = {
         method: "PUT",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(body)
-      }
-      const response  = await fetch(uri, options);
-      console.log(response)
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      };
+      const response = await fetch(uri, options);
+      console.log(response);
       if (response) {
-        dispatch({type: "EDIT_CARD", book, deck, korean, english, pronunciation, hanja, onmaster})
+        dispatch({
+          type: "EDIT_CARD",
+          book,
+          deck,
+          korean,
+          english,
+          pronunciation,
+          hanja,
+          onmaster,
+        });
       }
     } catch (err) {
       console.error(err.message);
     }
-  }
+  };
 
   const deleteCard = async (card_id) => {
     try {
@@ -56,15 +74,38 @@ const Card = (props) => {
 
   return (
     <tr>
-      <td><InlineEdit text={book} setText={setBook}/></td>
-      <td><InlineEdit text={deck} setText={setDeck}/></td>
-      <td><InlineEdit text={korean} setText={setKorean}/></td>
-      <td><InlineEdit text={english} setText={setEnglish}/></td>
-      <td><InlineEdit text={pronunciation} setText={setPronunciation}/></td>
-      <td><InlineEdit text={hanja} setText={setHanja}/></td>
-      <td><input type="checkbox" checked={onmaster} onChange={() => setOnMaster(!onmaster)}/></td>
       <td>
-        <button onClick={() => deleteCard(card.card_id)}>Delete</button>
+        <InlineEdit text={book} setText={setBook} />
+      </td>
+      <td>
+        <InlineEdit text={deck} setText={setDeck} />
+      </td>
+      <td>
+        <InlineEdit text={korean} setText={setKorean} />
+      </td>
+      <td>
+        <InlineEdit text={english} setText={setEnglish} />
+      </td>
+      <td>
+        <InlineEdit text={pronunciation} setText={setPronunciation} />
+      </td>
+      <td>
+        <InlineEdit text={hanja} setText={setHanja} />
+      </td>
+      <td>
+        <div className="inline-container">
+          <input
+            type="checkbox"
+            checked={onmaster}
+            onChange={() => setOnMaster(!onmaster)}
+          />
+        </div>
+      </td>
+
+      <td>
+        <div className="inline-container">
+          <button onClick={() => deleteCard(card.card_id)}>Delete</button>
+        </div>
       </td>
     </tr>
   );
