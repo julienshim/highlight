c0006afb989f2ff3e0daebf396dd7d5b1061c6f4
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-10-09 21:00:23 -0700

    remove key

--- client/src/components/CardsList/CardsList.js
+++ client/src/components/CardsList/CardsList.js
@@ -1,11 +1,10 @@
 import React, { Fragment, useContext } from "react";
 import Card from "../Card/Card";
 import CardsContext from "../../context/cards-context";
-import './CardsList.scss';
+import "./CardsList.scss";
 
 const ListCards = () => {
   const { cards } = useContext(CardsContext);
-
   return (
     <Fragment>
       <h1>Vocabulary List</h1>
@@ -23,7 +22,10 @@ const ListCards = () => {
         </thead>
         <tbody>
           {cards.map((card) => (
-            <Card key={`card-${card.card_id}`} card={card} />
+            <Card
+              key={`card-${card.card_id}`}
+              card={card}
+            />
           ))}
         </tbody>
       </table>
