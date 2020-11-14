339d530f465f07e4ef17b85fbe0a3f8d5a8d0c34
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-13 18:36:31 -0800

    init template comp

---
+++ src/Components/Templates.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import templateData from "../templateData";
+import {Link} from 'react-router-dom';
+
+const Templates = () => {
+  const [templates, setTemplates] = useState(templateData);
+  return (
+    <div>
+      <h1>Templates</h1>
+      <div>
+      <Link to="/templates/add">Create a New Template</Link>
+
+      </div>
+      <div>
+        <h2>Saved Templates</h2>
+        {templates.map((template) => (
+          <div>{template.study}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Templates;
