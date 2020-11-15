87da29b12176d881c90706da5d9fbd1d8fc396f6
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-14 20:11:23 -0800

    i don't know what changed here?

--- src/Components/Templates.js
+++ src/Components/Templates.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import templateData from "../templateData";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Templates = () => {
   const [templates, setTemplates] = useState(templateData);
@@ -8,13 +8,12 @@ const Templates = () => {
     <div>
       <h1>Templates</h1>
       <div>
-      <Link to="/templates/add">Create a New Template</Link>
-
+        <Link to="/templates/add">Create a New Template</Link>
       </div>
       <div>
         <h2>Saved Templates</h2>
-        {templates.map((template) => (
-          <div>{template.study}</div>
+        {templates.map((template, index) => (
+          <div key={`template-${index}`}>{template.study}</div>
         ))}
       </div>
     </div>
