cdd7593a059c4a951f7fa610ab976076e1bea32a
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-15 20:46:39 -0800

    add reducers + context

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import templateData from "../templateData";
 
+// components
 import Home from "./Home";
 import Templates from "./Templates";
-import CreateTemplate from "./CreateTemplate";
+import AddTemplate from "./AddTemplate";
+// import UpdateTemplate from "./UpdateTemplate";
 import Notes from "./Notes";
+
+// reducers
+import templatesReducer from "../reducers/templatesReducer";
+
+// context
+import TemplatesContext from "../context/templates-context";
 
 const routes = [
   {
@@ -20,22 +29,38 @@ const routes = [
   {
     path: "/templates/add",
     exact: true,
-    component: () => <CreateTemplate />,
+    component: () => <AddTemplate />,
   },
+  // {
+  //   path: "/templates/update/:id",
+  //   exact: true,
+  //   component: () => <UpdateTemplate />,
+  // },
   {
     path: "/notes",
     component: () => <Notes />,
   },
   {
-    path: "/",
+    path: "*",
     component: () => <div>404</div>,
   },
 ];
 
 export default function App() {
+  const [templates, dispatch] = useReducer(templatesReducer, []);
+
+  const getTemplates = () => {
+    dispatch({ type: "POPULATE_TEMPLATES", templates: templateData });
+  };
+
+  useEffect(() => {
+    // console.log(templateData)
+    getTemplates();
+  }, []);
+
   return (
     <Router>
-      <div style={{ display: "flex", height: "100vh" }}>
+      <div style={{ display: "flex", minHeight: "100vh" }}>
         <div
           style={{
             padding: "10px",
@@ -57,16 +82,18 @@ export default function App() {
         </div>
 
         <div style={{ padding: "24px 48px", width: "100%" }}>
-          <Switch>
-            {routes.map((route, index) => (
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.component}
-              />
-            ))}
-          </Switch>
+          <TemplatesContext.Provider value={{ templates, dispatch }}>
+            <Switch>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              ))}
+            </Switch>
+          </TemplatesContext.Provider>
         </div>
       </div>
     </Router>
