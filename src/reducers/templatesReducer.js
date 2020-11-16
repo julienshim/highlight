01331250a662d5735ec84d4ecd77d74d63a3f9ef
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-15 20:47:43 -0800

    reducers init

---
+++ src/reducers/templatesReducer.js
@@ -0,0 +1,25 @@
+const templatesReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_TEMPLATES":
+      return action.templates;
+    case "REMOVE_TEMPLATE":
+      return state.filter((template, templateIndex) => templateIndex !== action.template_id);
+    case "ADD_TEMPLATE":
+      return [
+        ...state,
+        {
+          title: action.title,
+          scenarios: action.scenarios,
+        },
+      ];
+    case "EDIT_TEMPLATE":
+      return state.map((template, templateIndex) => templateIndex === action.template_id ? {
+        title: action.title,
+        scenarios: action.scenarios
+      } : template)
+    default:
+      return state;
+  }
+};
+
+export default templatesReducer;
