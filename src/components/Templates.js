8149bd9c55c8689bf979e55cc54dc23788285df6
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-17 22:19:58 -0800

    addicon id to notes icon class

--- src/components/Templates.js
+++ src/components/Templates.js
@@ -20,7 +20,7 @@ const Templates = () => {
 
   const addIcon = (
     <svg
-      id="addIcon"
+      className="add-icon"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
