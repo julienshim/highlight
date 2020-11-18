4f9a68adf74dbebefdce84bad6129467b133cef9
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-11-17 19:00:17 -0800

    notesreducer complete

---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,30 @@
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_NOTES":
+      return action.notes;
+    case "REMOVE_NOTE":
+      return state.filter((notes, notesIndex) => notesIndex !== action.notes_refId);
+    case "ADD_NOTE":
+      return [
+        {
+          refId: action.refId,
+          header: action.header,
+          body: action.body,
+          footer: action.footer
+        },
+        ...state
+      ];
+    case "EDIT_NOTE":
+      return state.map((note, noteIndex) => noteIndex === action.notes_refId ? {
+        refId: action.refId,
+        header: action.header,
+        body: action.body,
+        footer: action.footer,
+        summary: action.summary
+      } : note)
+    default:
+      return state;
+  }
+};
+
+export default notesReducer;
