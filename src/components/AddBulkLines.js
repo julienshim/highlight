00809f5490f3102a8b3aa90f473d3858aad15b84
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-12-30 17:08:11 -0800

    add bulk lines init

---
+++ src/components/AddBulkLines.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const AddBulkLines = () => {
+ return (
+   <div style={{
+     position: "absolute",
+     top: 0,
+     left: 0,
+     backgroundColor: "rgba(30, 30, 30, 0.5)",
+     width: "100vw",
+     height: "100vh",
+   }}>
+     <h2>Add Bulk Lines</h2>
+     <textarea/>
+   </div>
+ )
+
+};
+
+export default AddBulkLines;
