189cb85a3a752e59cd237f7fd89785337035d926
Julien Shim <25723428+julienshim@users.noreply.github.com>
2021-01-04 21:23:36 -0800

    trash icon init

--- src/components/Templates.js
+++ src/components/Templates.js
@@ -9,6 +9,7 @@ const Templates = () => {
 
   const editIcon = (
     <svg
+      className="edit-icon"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -30,6 +31,10 @@ const Templates = () => {
     </svg>
   );
 
+  const trashIcon = (
+    <svg className="trash-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
+  )
+
   return (
     <div>
       <h1>Templates</h1>
@@ -44,7 +49,7 @@ const Templates = () => {
       <h3>Saved Templates</h3>
       {(templates.length === 0) && (
         <div>
-          <p style={{color: "var(--dogwood-rose)", fontWeight: "bold"}}>
+          <p style={{ color: "var(--dogwood-rose)", fontWeight: "bold" }}>
             No saved templates found.
           </p>
         </div>
@@ -105,17 +110,20 @@ const Templates = () => {
                       alignItems: "center",
                       height: "100%",
                       justifyContent: "center",
-                      cursor: "pointer"
                     }}
                   >
                     <div
-                      style={{}}
+                      style={{
+                        cursor: "pointer",
+                        margin: "0 24px"
+                      }}
                       onClick={() =>
                         history.push(`/templates/update/${templateIndex}`)
                       }
                     >
                       {editIcon}
                     </div>
+                    <div style={{cursor: "pointer", margin: "0 24px"}}>{trashIcon}</div>
                   </div>
                 </div>
               </div>
