a2970b42dcde5c2129c66ac6952174746a624a5e
Julien Shim <25723428+julienshim@users.noreply.github.com>
2021-02-09 20:25:59 -0800

    checklists reducer init

---
+++ src/reducers/checklistsReducer.js
@@ -0,0 +1,25 @@
+const checklistsReducer = (state, action) => {
+  switch (action.type) {
+    case "POPULATE_CHECKLISTS":
+      return action.checklists;
+    case "REMOVE_CHECKLIST":
+      return state.filter((checklist, checklistIndex) => checklistIndex !== action.checklist_id);
+    case "ADD_CHECKLIST":
+      return [
+        {
+          title: action.title,
+          scenarios: action.scenarios,
+        },
+        ...state,
+      ];
+    case "EDIT_CHECKLIST":
+      return state.map((checklist, checklistIndex) => checklistIndex === action.checklist_id ? {
+        title: action.title,
+        scenarios: action.scenarios
+      } : checklist)
+    default:
+      return state;
+  }
+};
+
+export default checklistsReducer;
