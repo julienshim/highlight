d6a0d0934f8e87f05ba708c7444c348be55279da
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-16 18:03:34 -0800

    linted

--- src/components/AddSectionButton.js
+++ src/components/AddSectionButton.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 const AddSectionButton = (props) => {
-    const {handleAddNewScenario} = props;
-    return (
-        <p
-        className="add-section-button"
-        style={{
-          display: "inline-block",
-          boxSizing: "border-box",
-          fontSize: "0.8rem",
-          padding: "12px 24px",
-          cursor: "pointer",
-          fontWeight: "bold",
-          marginLeft: "72px",
-          border: "2px solid var(--blue-munsell)",
-        }}
-        onClick={() => handleAddNewScenario()}
-      >
-        New Section
-      </p>
-    )
+  const { handleAddNewScenario } = props;
+  return (
+    <div
+      className="add-section-button"
+      style={{
+        display: 'inline-block',
+        boxSizing: 'border-box',
+        fontSize: '0.8rem',
+        padding: '12px 24px',
+        cursor: 'pointer',
+        fontWeight: 'bold',
+        marginLeft: '72px',
+        border: '2px solid var(--blue-munsell)',
+      }}
+      onClick={() => handleAddNewScenario()}
+      onKeyUp={() => handleAddNewScenario()}
+      role="button"
+      tabIndex={0}
+    >
+      New Section
+    </div>
+  );
 };
 
 export default AddSectionButton;
