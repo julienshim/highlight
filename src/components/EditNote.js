6fa69710a10c3500fbbdd7d9407a02c207fecf3f
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-20 14:24:40 -0800

    linted

--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -1,12 +1,12 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import NotesContext from "../context/notes-context";
-import InlineEditIdentifier from "./InlineEditIdentifier";
-import CommentSubtitle from "./CommentSubtitle";
-import CommentContent from "./CommentContent";
-import CommentHeader from "./CommentHeader";
-import CommentFooter from "./CommentFooter";
-import DeleteButton from "./DeleteButton";
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import NotesContext from '../context/notes-context';
+import InlineEditIdentifier from './InlineEditIdentifier';
+import CommentSubtitle from './CommentSubtitle';
+import CommentContent from './CommentContent';
+import CommentHeader from './CommentHeader';
+import CommentFooter from './CommentFooter';
+import DeleteButton from './DeleteButton';
 import SaveButton from './SaveButton';
 
 const EditNote = () => {
@@ -15,12 +15,12 @@ const EditNote = () => {
   const history = useHistory();
   const { id } = useParams();
 
-  const [refId, setRefId] = useState("Unidentified");
-  const [header, setHeader] = useState("");
+  const [refId, setRefId] = useState('Unidentified');
+  const [header, setHeader] = useState('');
   const [title, setTitle] = useState([]);
   const [scenarios, setScenarios] = useState([]);
-  const [footer, setFooter] = useState("");
-  const [summary, setSummary] = useState("");
+  const [footer, setFooter] = useState('');
+  const [summary, setSummary] = useState('');
   const [isSaved, setIsSaved] = useState(false);
 
   useEffect(() => {
@@ -34,15 +34,15 @@ const EditNote = () => {
   }, [id, notes]);
 
   useEffect(() => {
-    setIsSaved(false)
-  },[footer, scenarios, title, header, refId])
+    setIsSaved(false);
+  }, [footer, scenarios, title, header, refId]);
 
   const addNote = (event) => {
     event.preventDefault();
     dispatchNotes({
-      type: "EDIT_NOTE",
-      note_id: parseInt(id),
-      refId: refId === "" ? "Unidentified" : refId,
+      type: 'EDIT_NOTE',
+      note_id: parseInt(id, 10),
+      refId: refId === '' ? 'Unidentified' : refId,
       header,
       body: {
         title,
@@ -58,7 +58,7 @@ const EditNote = () => {
   useEffect(() => {
     const filteredScenarios = scenarios.filter(
       (scenario) =>
-        scenario.subtitleComments !== "" ||
+        scenario.subtitleComments !== '' ||
         scenario.entries.filter((entry) => entry.contentComments).length > 0
     );
     if (filteredScenarios) {
@@ -72,12 +72,12 @@ const EditNote = () => {
           const flattenedEntries = entries
             .filter((entry) => entry.contentComments)
             .map((entry) => `${entry.content} - ${entry.contentComments}`)
-            .join(" ");
+            .join(' ');
           statement = [...statement, flattenedEntries];
-          return statement.join(" ");
+          return statement.join(' ');
         })
-        .join("\n\n");
-      setSummary([header, newSummary, footer].filter((x) => x).join("\n\n"));
+        .join('\n\n');
+      setSummary([header, newSummary, footer].filter((x) => x).join('\n\n'));
     }
   }, [scenarios, header, footer]);
 
@@ -95,19 +95,19 @@ const EditNote = () => {
 
   const handleDeleteNote = () => {
     dispatchNotes({
-      type: "REMOVE_NOTE",
-      note_refId: parseInt(id),
+      type: 'REMOVE_NOTE',
+      note_refId: parseInt(id, 10),
     });
-    history.push("/notes");
-  }
+    history.push('/notes');
+  };
 
   return (
     <div>
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
         <InlineEditIdentifier text={refId} setText={setRefId} />
-        <h3
-          style={{ color: "var(--english-violet)", padding: "8px 12px" }}
-        >{`(${title})`}</h3>
+        <h3 style={{ color: 'var(--english-violet)', padding: '8px 12px' }}>
+          {`(${title})`}
+        </h3>
       </div>
       <div>
         <CommentHeader text={header} setText={setHeader} />
@@ -115,7 +115,7 @@ const EditNote = () => {
       {scenarios.map((scenario, scenarioIndex) => {
         const { subtitle, subtitleComments, entries } = scenario;
         return (
-          <div key={`scenario-${scenarioIndex}`}>
+          <div key={`scenario-${scenarioIndex * Date.now()}`}>
             <CommentSubtitle
               text={subtitleComments}
               subtitle={subtitle}
@@ -127,7 +127,7 @@ const EditNote = () => {
               {entries.map((entry, entryIndex) => {
                 const { content, contentComments } = entry;
                 return (
-                  <div key={`entry${entryIndex}`}>
+                  <div key={`entry${entryIndex * Date.now()}`}>
                     <CommentContent
                       text={contentComments}
                       setText={handleContentCommentsChange}
@@ -148,12 +148,12 @@ const EditNote = () => {
 
       <textarea
         style={{
-          height: "250px",
-          width: "100%",
-          marginTop: "12px",
-          border: "1px solid green",
-          boxSizing: "border-box",
-          overflowY: "scroll",
+          height: '250px',
+          width: '100%',
+          marginTop: '12px',
+          border: '1px solid green',
+          boxSizing: 'border-box',
+          overflowY: 'scroll',
         }}
         defaultValue={summary}
         readOnly
@@ -161,7 +161,7 @@ const EditNote = () => {
       <form onSubmit={addNote}>
         <SaveButton isSaved={isSaved} />
       </form>
-      <DeleteButton deleteAction={handleDeleteNote} type='note' />
+      <DeleteButton deleteAction={handleDeleteNote} type="note" />
     </div>
   );
 };
