c46dae75e410db7cbb066343466179d5ffa0cb68
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-20 14:23:56 -0800

    bootstrap init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React, {StrictMode} from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <StrictMode>
