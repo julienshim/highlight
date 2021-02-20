f4a79e342ec85a7aa9f85d2b9a1d4c0e1afb27e5
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-20 14:45:03 -0800

    prettified

--- src/sampleData.js
+++ src/sampleData.js
@@ -1,53 +1,62 @@
 const sampleTemplate = [
   {
-    title: "German 1, Extra Credit 1, O Tannenbaum",
+    title: 'German 1, Extra Credit 1, O Tannenbaum',
     scenarios: [
       {
-        subtitle: "O Tannenbaum (Section 1)",
-        subtitleComments: "",
+        subtitle: 'O Tannenbaum (Section 1)',
+        subtitleComments: '',
         entries: [
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Wie treu sind deine Blätter!"', contentComments: "" },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          { content: '"Wie treu sind deine Blätter!"', contentComments: '' },
           {
             content: '"Du grünst nicht nur zur Sommerzeit,"',
-            contentComments: "",
+            contentComments: '',
           },
           {
             content: '"Nein, auch im Winter, wenn es schneit."',
-            contentComments: "",
+            contentComments: '',
           },
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Wie treu sind deine Blätter!"', contentComments: "" },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          { content: '"Wie treu sind deine Blätter!"', contentComments: '' },
         ],
       },
       {
-        subtitle: "O Tannenbaum (Section 2)",
-        subtitleComments: "",
+        subtitle: 'O Tannenbaum (Section 2)',
+        subtitleComments: '',
         entries: [
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Du kannst mir sehr gefallen!"', contentComments: "" },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          { content: '"Du kannst mir sehr gefallen!"', contentComments: '' },
           {
             content: '"Wie oft hat nicht zur Weihnachtszeit"',
-            contentComments: "",
+            contentComments: '',
           },
           {
             content: '"Ein Baum von dir mich hoch erfreut!"',
-            contentComments: "",
+            contentComments: '',
           },
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Du kannst mir sehr gefallen!"', contentComments: "" },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          { content: '"Du kannst mir sehr gefallen!"', contentComments: '' },
         ],
       },
       {
-        subtitle: "O Tannenbaum (Section 3)",
-        subtitleComments: "",
+        subtitle: 'O Tannenbaum (Section 3)',
+        subtitleComments: '',
         entries: [
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Dein Kleid will mich was lehren:"', contentComments: "" },
-          { content: '"Die Hoffnung und Beständigkeit"', contentComments: "" },
-          { content: '"Gibt Mut und Kraft zu jeder Zeit!"', contentComments: "" },
-          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-          { content: '"Dein Kleid will mich was lehren!"', contentComments: "" },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          {
+            content: '"Dein Kleid will mich was lehren:"',
+            contentComments: '',
+          },
+          { content: '"Die Hoffnung und Beständigkeit"', contentComments: '' },
+          {
+            content: '"Gibt Mut und Kraft zu jeder Zeit!"',
+            contentComments: '',
+          },
+          { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+          {
+            content: '"Dein Kleid will mich was lehren!"',
+            contentComments: '',
+          },
         ],
       },
     ],
@@ -56,63 +65,76 @@ const sampleTemplate = [
 
 const sampleNote = [
   {
-    refId: "Pierre Paul Jacques",
+    refId: 'Pierre Paul Jacques',
     header: "Didn't spend time memrizing the songs. Referencing songsheet.",
     body: {
-      title: "German 1, Extra Credit 1, O Tannenbaum",
+      title: 'German 1, Extra Credit 1, O Tannenbaum',
       scenarios: [
         {
-          subtitle: "O Tannenbaum (Section 1)",
-          subtitleComments: "Great job with the first section!",
+          subtitle: 'O Tannenbaum (Section 1)',
+          subtitleComments: 'Great job with the first section!',
           entries: [
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Wie treu sind deine Blätter!"', contentComments: "" },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            { content: '"Wie treu sind deine Blätter!"', contentComments: '' },
             {
               content: '"Du grünst nicht nur zur Sommerzeit,"',
-              contentComments: "",
+              contentComments: '',
             },
             {
               content: '"Nein, auch im Winter, wenn es schneit."',
-              contentComments: "",
+              contentComments: '',
             },
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Wie treu sind deine Blätter!"', contentComments: "" },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            { content: '"Wie treu sind deine Blätter!"', contentComments: '' },
           ],
         },
         {
-          subtitle: "O Tannenbaum (Section 2)",
-          subtitleComments: "",
+          subtitle: 'O Tannenbaum (Section 2)',
+          subtitleComments: '',
           entries: [
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Du kannst mir sehr gefallen!"', contentComments: "" },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            { content: '"Du kannst mir sehr gefallen!"', contentComments: '' },
             {
               content: '"Wie oft hat nicht zur Weihnachtszeit"',
               contentComments: "Has trouble pronouncing 'Weihnachtszeit'.",
             },
             {
               content: '"Ein Baum von dir mich hoch erfreut!"',
-              contentComments: "",
+              contentComments: '',
             },
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Du kannst mir sehr gefallen!"', contentComments: "" },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            { content: '"Du kannst mir sehr gefallen!"', contentComments: '' },
           ],
         },
         {
-          subtitle: "O Tannenbaum (Section 3)",
-          subtitleComments: "",
+          subtitle: 'O Tannenbaum (Section 3)',
+          subtitleComments: '',
           entries: [
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Dein Kleid will mich was lehren:"', contentComments: "" },
-            { content: '"Die Hoffnung und Beständigkeit"', contentComments: "" },
-            { content: '"Gibt Mut und Kraft zu jeder Zeit!"', contentComments: "" },
-            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: "" },
-            { content: '"Dein Kleid will mich was lehren!"', contentComments: "" },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            {
+              content: '"Dein Kleid will mich was lehren:"',
+              contentComments: '',
+            },
+            {
+              content: '"Die Hoffnung und Beständigkeit"',
+              contentComments: '',
+            },
+            {
+              content: '"Gibt Mut und Kraft zu jeder Zeit!"',
+              contentComments: '',
+            },
+            { content: '"O Tannenbaum, o Tannenbaum,"', contentComments: '' },
+            {
+              content: '"Dein Kleid will mich was lehren!"',
+              contentComments: '',
+            },
           ],
         },
       ],
     },
-    footer: "Wants to apply the extra credit to Homework 12 in Week 6.",
-    summary: "Didn't spend time memrizing the songs. Referencing songsheet.\n\nO Tannenbaum (Section 1) - Great job with the first section! \n\nO Tannenbaum (Section 2) - \"Wie oft hat nicht zur Weihnachtszeit\" Has trouble pronouncing 'Weihnachtszeit'.\n\nWants to apply the extra credit to Homework 12 in Week 6.",
+    footer: 'Wants to apply the extra credit to Homework 12 in Week 6.',
+    summary:
+      "Didn't spend time memrizing the songs. Referencing songsheet.\n\nO Tannenbaum (Section 1) - Great job with the first section! \n\nO Tannenbaum (Section 2) - \"Wie oft hat nicht zur Weihnachtszeit\" Has trouble pronouncing 'Weihnachtszeit'.\n\nWants to apply the extra credit to Homework 12 in Week 6.",
   },
 ];
 
@@ -120,8 +142,8 @@ const sampleChecklist = [
   {
     title: 'Checklist A',
     subTitle: '123A',
-    body: '* 1\n* 2'
-  }
-]
+    body: '* 1\n* 2',
+  },
+];
 
 export { sampleTemplate, sampleNote, sampleChecklist };
