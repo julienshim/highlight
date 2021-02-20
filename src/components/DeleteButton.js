6d595c8a5a4bbeb42a1477d03075e1c4c60b84e1
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-20 14:25:10 -0800

    linted

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState } from 'react';
 
 const DeleteButton = (props) => {
   const { deleteAction, type } = props;
@@ -7,53 +7,62 @@ const DeleteButton = (props) => {
   return (
     <Fragment>
       {!isConfirming ? (
-        <p
+        <div
           className="delete-button"
           style={{
-            marginTop: "50px",
-            cursor: "pointer",
-            color: "var(--dogwood-rose)",
+            marginTop: '50px',
+            cursor: 'pointer',
+            color: 'var(--dogwood-rose)',
           }}
           onClick={() => setIsConfirming(true)}
+          onKeyUp={() => setIsConfirming(true)}
+          role="button"
+          tabIndex={0}
         >
-          Delete {type.slice(0,1).toUpperCase() + type.slice(1)}
-        </p>
+          Delete {type.slice(0, 1).toUpperCase() + type.slice(1)}
+        </div>
       ) : (
-        <div style={{ marginTop: "50px" }}>
+        <div style={{ marginTop: '50px' }}>
           <p
             style={{
-              fontWeight: "bold",
-              color: "var(--dogwood-rose)",
+              fontWeight: 'bold',
+              color: 'var(--dogwood-rose)',
             }}
           >
             Delete this {type}?
           </p>
-          <div style={{ display: "flex", cursor: "pointer" }}>
-            <p
+          <div style={{ display: 'flex', cursor: 'pointer' }}>
+            <div
               className="confirm-button"
               style={{
-                padding: "12px 24px",
-                fontSize: "0.8rem",
-                margin: "0 3px",
-                backgroundColor: "var(--cadet-grey)",
+                padding: '12px 24px',
+                fontSize: '0.8rem',
+                margin: '0 3px',
+                backgroundColor: 'var(--cadet-grey)',
               }}
               onClick={() => setIsConfirming(false)}
+              onKeyUp={() => setIsConfirming(false)}
+              role="button"
+              tabIndex={0}
             >
               Cancel
-            </p>
-            <p
+            </div>
+            <div
               className="confirm-button"
               style={{
-                padding: "12px 24px",
-                fontSize: "0.8rem",
-                margin: "0 3px",
-                color: "white",
-                backgroundColor: "var(--dogwood-rose)",
+                padding: '12px 24px',
+                fontSize: '0.8rem',
+                margin: '0 3px',
+                color: 'white',
+                backgroundColor: 'var(--dogwood-rose)',
               }}
               onClick={() => deleteAction()}
+              onKeyUp={() => deleteAction()}
+              role="button"
+              tabIndex={0}
             >
               Yes, delete it.
-            </p>
+            </div>
           </div>
         </div>
       )}
