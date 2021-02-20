318d73029d02bf46e1084d931fa5802dcca386bd
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-19 22:34:28 -0800

    linted

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const SaveButton = (props) => {
-    const { isSaved } = props;
+  const { isSaved } = props;
 
-    const savedStyle = { 
-        backgroundColor: "var(--english-violet)",
-        color: "white"
-      }
-    return (
-        <input className="submit-button" style={isSaved ? savedStyle : {}} type="submit" value={isSaved ? "Changes Saved" : "Save"} />
-    )
+  const savedStyle = {
+    backgroundColor: 'var(--english-violet)',
+    color: 'white',
+  };
+  return (
+    <input
+      className="submit-button"
+      style={isSaved ? savedStyle : {}}
+      type="submit"
+      value={isSaved ? 'Changes Saved' : 'Save'}
+    />
+  );
 };
 
 export default SaveButton;
