a7aba1b93f58f441434e9779056388684a278b2e
julienshim <25723428+julienshim@users.noreply.github.com>
2021-02-20 14:25:20 -0800

    linted

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
 const Home = () => {
   return (
@@ -12,26 +12,47 @@ const Home = () => {
       </p>
       <h2>Getting Started</h2>
       <h3>Templates</h3>
-      <p>By clicking on 'Templates' on the top navigation bar, users can view 'Saved Templates' or 'Create a New Template'. Templates are structured as follows:</p>
+      <p>
+        By clicking on &apos;Templates&apos; on the top navigation bar, users
+        can view &apos;Saved Templates&apos; or &apos;Create a New
+        Template&apos;. Templates are structured as follows:
+      </p>
       <ul>
         <li>Template Title</li>
         <li>Section Subtitle</li>
         <li>Line Text</li>
       </ul>
-      <p>Users can multiple sections, as well as add multiple lines of text in each section.</p>
+      <p>
+        Users can multiple sections, as well as add multiple lines of text in
+        each section.
+      </p>
       <h3>Notes</h3>
-      <p>By clicking on 'Notes' on the top navigation bar, users can view 'Saved Notes' or 'Create a New Note'. Notes are structured as follows:</p>
+      <p>
+        By clicking on &apos;Notes&apos; on the top navigation bar, users can
+        view &apos;Saved Notes&apos; or &apos;Create a New Note&apos;. Notes are
+        structured as follows:
+      </p>
       <ul>
         <li>Note Identifier</li>
         <li>Template Selection</li>
       </ul>
-      <p>Users can then click line subtitles or lines of text and comment in the yellow input boxes. A summary of clicked on and commented text will automatically generate at the bottom of the note. Summaries are structued as follows depending if an item is commented on:</p>
-        <ul>
-          <li>{"<Header>"}</li>
-          <li>{"<Subtitle>: <Subtitle Comment> <Line Text> - <Line Text Comment>"}</li>
-          <li>{"<Footer>"}</li>
-        </ul>
-        <p>Comments are unpunctuated for flexibility on user's preferences with separaters.</p>
+      <p>
+        Users can then click line subtitles or lines of text and comment in the
+        yellow input boxes. A summary of clicked on and commented text will
+        automatically generate at the bottom of the note. Summaries are
+        structued as follows depending if an item is commented on:
+      </p>
+      <ul>
+        <li>{'<Header>'}</li>
+        <li>
+          {'<Subtitle>: <Subtitle Comment> <Line Text> - <Line Text Comment>'}
+        </li>
+        <li>{'<Footer>'}</li>
+      </ul>
+      <p>
+        Comments are unpunctuated for flexibility on user&apos;s preferences with
+        separaters.
+      </p>
       <h2>Updates</h2>
     </Fragment>
   );
